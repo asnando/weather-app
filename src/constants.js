86b7672971f3bcdf54cd9f894189796384f7cc91
Fernando Rama <fernandorama.dev@gmail.com>
2020-07-03 03:20:12 -0300

    use https for images

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const OPENWEATHER_API_BASE_URL = 'https://api.openweathermap.org/data/2.5';
-export const OPENWEATHER_ICON_BASE_URL = 'http://openweathermap.org/img/wn/$iconName@2x.png';
+export const OPENWEATHER_ICON_BASE_URL = 'https://openweathermap.org/img/wn/$iconName@2x.png';
 export const OPENWEATHER_LANG = 'pt_br';
 export const OPENWEATHER_UNITS_TYPE = 'metric';
 export const MOMENTJS_LANG = 'pt-br';
