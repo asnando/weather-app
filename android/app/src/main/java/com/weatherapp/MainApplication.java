4d6b8345e6785a93591ac33141a99a6136cfd9f7
Fernando Rama <fernandorama.dev@gmail.com>
2020-07-03 03:03:05 -0300

    update simple colored splashscreen and icon

--- android/app/src/main/java/com/weatherapp/MainApplication.java
+++ android/app/src/main/java/com/weatherapp/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.lottie.LottiePackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -11,7 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.airbnb.android.react.lottie.LottiePackage;
+// import com.airbnb.android.react.lottie.LottiePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,7 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new LottiePackage());
+          // packages.add(new LottiePackage());
           return packages;
         }
 
